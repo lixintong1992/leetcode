1e92125a6a5cfd086dd3e8c97974778572d0f010
lixintong1992 <18392181430@163.com>
2016-02-27 22:51:19 +0800

    using n&(n-1) trick

---
+++ python/231.py
@@ -0,0 +1,10 @@
+# 231. Power of Two
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        return not (n & (n-1))
