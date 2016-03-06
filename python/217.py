2d0635c62547ba96555fde74688e481ff163c150
lixintong1992 <18392181430@163.com>
2016-03-06 20:46:44 +0800

    easy

---
+++ python/217.py
@@ -0,0 +1,8 @@
+# 217. Contains Duplicate
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
