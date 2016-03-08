58f1987f7133c1d3976e0c7cb1a6911301eecb66
lixintong1992 <18392181430@163.com>
2016-03-08 20:09:01 +0800

    not easy about tree

---
+++ python/257.py
@@ -0,0 +1,25 @@
+# 257. Binary Tree Paths
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+# dfs + stack
+def binaryTreePaths1(self, root):
+    if not root:
+        return []
+    res, stack = [], [(root, "")]
+    while stack:
+        node, ls = stack.pop()
+        if not node.left and not node.right:
+            res.append(ls+str(node.val))
+        if node.right:
+            stack.append((node.right, ls+str(node.val)+"->"))
+        if node.left:
+            stack.append((node.left, ls+str(node.val)+"->"))
+    return res
