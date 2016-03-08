09d9d3ac85372a99fa9d98b4d4eda280af98a0b8
lixintong1992 <18392181430@163.com>
2016-03-08 20:09:50 +0800

    alter

--- python/257.py
+++ python/257.py
@@ -10,16 +10,16 @@ class TreeNode(object):
 
 
 # dfs + stack
-def binaryTreePaths1(self, root):
-    if not root:
-        return []
-    res, stack = [], [(root, "")]
-    while stack:
-        node, ls = stack.pop()
-        if not node.left and not node.right:
-            res.append(ls+str(node.val))
-        if node.right:
-            stack.append((node.right, ls+str(node.val)+"->"))
-        if node.left:
-            stack.append((node.left, ls+str(node.val)+"->"))
-    return res
+    def binaryTreePaths1(self, root):
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        while stack:
+            node, ls = stack.pop()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.right:
+                stack.append((node.right, ls+str(node.val)+"->"))
+            if node.left:
+                stack.append((node.left, ls+str(node.val)+"->"))
+        return res
