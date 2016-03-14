6443797ac240d08bd3b11128ff8bba46ec291dd8
lixintong1992 <18392181430@163.com>
2016-03-14 21:09:58 +0800

    math trick

---
+++ python/172.py
@@ -0,0 +1,12 @@
+# 172. Factorial Trailing Zeroes
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        zeroCnt = 0
+        while n > 0:
+            n = n/5
+            zeroCnt += n
+        return zeroCnt
