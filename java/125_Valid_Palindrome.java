be04fdae3fe6a4b3e21909ee634fa59f605a9f73
lixintong1992 <18392181430@163.com>
2016-10-06 20:47:17 +0800

    1_WAP

--- java/125_Valid_Palindrome.java
+++ java/125_Valid_Palindrome.java
@@ -1,9 +1,10 @@
 public class Solution {
     public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
         for (int i = 0, j = s.length() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
         while (!Character.isLetterOrDigit(s.charAt(i)) && i < j) i++; // Increment left pointer if not alphanumeric
         while (!Character.isLetterOrDigit(s.charAt(j)) && i < j) j--; // Decrement right pointer if no alphanumeric
-        if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false; // Exit and return error if not match
+        if (s.charAt(i) != s.charAt(j)) return false; // Exit and return error if not match
         }
     return true;
     }
