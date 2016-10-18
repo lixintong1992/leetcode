2fe1df321dafe115fc61513735bc80576229c3d7
wanxing <wanxing21@sina.com>
2016-10-18 23:40:41 +0800

    WAP

---
+++ java/Permutations.java
@@ -0,0 +1,48 @@
+package test;
+
+class Solution {
+	public int run() {
+		System.out.println("asd");
+		return 1;
+	}
+}
+
+public class Test {
+	public static void permutate(String input) {
+		if (input == null)
+			throw new IllegalArgumentException();
+		char[] data = input.toCharArray();
+		permutate(data, 0);
+	}
+
+	public static void permutate(char[] data, int begin) {
+		int length = data.length;
+		if (begin == length)
+			System.out.println(data);
+		for (int i = begin; i < length; i++) {
+			if (isUnique(data, begin, i)) {
+				swap(data, begin, i);
+				permutate(data, begin + 1);
+				swap(data, begin, i);
+			}
+		}
+	}
+
+	private static boolean isUnique(char[] data, int begin, int end) {
+		for (int i = begin; i < end; i++)
+			if (data[i] == data[end])
+				return false;
+		return true;
+	}
+
+	private static void swap(char[] data, int left, int right) {
+		char temp = data[left];
+		data[left] = data[right];
+		data[right] = temp;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test.permutate("abbc");
+	}
+}
