295d42708a808367707153abe079e66ef257d679
lixintong <18392181430@163.com>
2016-11-01 22:30:06 +0800

    just count

---
+++ java/400_Nth_Digit.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findNthDigit(int n) {
+        int k = 1;
+        long base = 1;
+
+        // find the corresponding k-digits array
+        while(n > base * 9 * k){
+            n -= base * 9 * k;
+            k++;
+            base = base * 10;
+        }
+        
+        // corresponding integer in the array
+        long value = base + (n-1)/k;
+        
+        // digit of the integer
+        return Long.toString(value).charAt((n-1)%k) - '0';
+    }
+}
