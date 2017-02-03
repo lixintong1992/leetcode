54fcd42e3ae41d5d196fe67dc301a94820ba7172
lixintong <18392181430@163.com>
2017-02-03 14:27:36 +0800

    ~

--- java/463_Island_Perimeter.java
+++ java/463_Island_Perimeter.java
@@ -1,18 +1,17 @@
 public class Solution {
     public int islandPerimeter(int[][] grid) {
-        int result = 0;
-        for (int i = 0; i < grid.length; i++){
-            for (int j = 0; j < grid[0].length; j++){
-                if (grid[i][j] == 1){
-                    if ((j==0)||(j==grid[0].length)||(grid[i][j-1]==0)||(grid[i][j+1]==0)){
-                        result++;
-                    }
-                    if ((i==0)||(i==grid.length)||(grid[i-1][j]==0)||(grid[i+1][j]==0)){
-                        result++;
-                    }
+        int islands = 0, neighbours = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    islands++; // count islands
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbours++; // count down neighbours
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbours++; // count right neighbours
                 }
             }
         }
-        return result;
+
+        return islands * 4 - neighbours * 2;
     }
 }
